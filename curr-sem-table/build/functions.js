ffe60b499e74917e2bf804408eb06427fb8c2431
Austin Chan <achan113@ucmerced.edu>
2019-11-30 22:08:47 -0800

    Added a delete row option

---
+++ curr-sem-table/build/functions.js
@@ -0,0 +1,17 @@
+function appendRow() {
+    var table = document.getElementById("middleTable");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+    cell3.innerHTML = "NEW CELL3";
+    cell4.innerHTML = "NEW CELL4";
+  }
+
+  function deleteRow() {
+    var table = document.getElementById("middleTable");
+    table.deleteRow(-1);
+  }
